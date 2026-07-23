ba2f35a47d986383ccde0667023fdf36a930d3bf
RridaFfatima <142243922+RridaFfatima@users.noreply.github.com>
2026-07-23 16:33:05 +0500

    arduino_codes

---
+++ ultrasound/ultrasound.ino
@@ -0,0 +1,21 @@
+int trigpin=8;
+int echopin=9;
+float duration, distance;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(trigpin,OUTPUT);
+pinMode(echopin,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trigpin,HIGH);
+delayMicroseconds(10);
+digitalWrite(trigpin,LOW);
+
+duration=pulseIn(echopin,HIGH);
+distance=(duration *0.034)/2;
+Serial.println(distance);
+delay(200);
+}
